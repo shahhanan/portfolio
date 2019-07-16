c21007d054bb5e82b0ea69ff03d53b0ccb5df662
Hanan Shabir <hanan.shabir@impelsys.com>
2019-07-16 11:29:02 +0530

    changes for Homepage

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -13,10 +13,19 @@ class Home extends Component {
         <div id="intro-container" className="flex-center">
           <div className="intro-card">
             <div className="card-container">
-              <ProfileImage   />
-              <InformationCard  />
-              <SocialIcons   />
-            </div> 
+              <ProfileImage />
+              <InformationCard />
+              <SocialIcons />
+            </div>
+          </div>
+          <div className="sideCard">
+            <div className="mini-about">I currently am working as a UI Developer at <a href="https://www.impelsys.com/">Impelsys,</a>  <br />
+              which is where my passion for development converge. 
+              See my previous work history on <a href="https://www.linkedin.com/in/hanan-shah-375a3a106/">LinkedIn.</a> <br />
+              Currently I am accepting freelance work, 
+              I'd love to get coffee and get to know you, 
+              so don’t hesitate to get in touch.
+              </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {
   faCode,
   faEnvelope
 } from "@fortawesome/free-solid-svg-icons";
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 library.add(fab, faHome, faUser, faCode, faEnvelope);
 import { HashRouter, Route, Switch } from "react-router-dom";
@@ -15,20 +15,20 @@ import { Provider } from "react-redux";
 import { createBrowserHistory } from "history";
 import configureStore from "./store";
 import ErrorBoundary from "./utils/ErrorBoundary";
-
 // Containers
 import Approuter from "./containers/Approuter";
-const store = configureStore();
+/* import Page404 from "./components/Page404/Page404";
+ */const store = configureStore();
 const history = createBrowserHistory();
 ReactDOM.render(
   <Provider store={store} key="provider">
     <ErrorBoundary>
       <HashRouter history={history}>
         <Switch>
-          {/* <Route exact path="/404" name="Page 404" component={Page404}/>
-    <Route exact path="/500" name="Page 500" component={Page500}/> */}
-          <Route path="" name="Home" component={Approuter} />
-          <Route path="/" name="Home" component={Approuter} />
+          {/* <Route exact path="/500" name="Page 500" component={Page500}/> */}
+          {/* <Route path="/404" name="Page404" component={Page404} />
+          <Route exact path="" name="Home2" component={Approuter} />  */}
+          <Route path="/" name="Home1" component={Approuter} />
         </Switch>
       </HashRouter>
     </ErrorBoundary>

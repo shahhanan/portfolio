df693c60f3be74f2be7204d3bbd1b00e4bcf64bf
Hanan Shabir <hanan.shabir@impelsys.com>
2019-07-19 12:30:36 +0530

    New changes for Page 404 and page 500

---
+++ src/containers/Helpers/Page500/Page500.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Page500 extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div className="clearfix">
+                <h1 className="float-left display-3 mr-4">500</h1>
+                <h4 className="pt-3">Houston, we have a problem!</h4>
+                <p className="text-muted">The page you are looking for is temporarily unavailable.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page500;
--- src/index.js
+++ src/index.js
@@ -17,19 +17,21 @@ import configureStore from "./store";
 import ErrorBoundary from "./utils/ErrorBoundary";
 // Containers
 import Approuter from "./containers/Approuter";
-/* import Page404 from "./components/Page404/Page404";
- */const store = configureStore();
+import Page404 from "./containers/Helpers/Page404/";
+import Page500 from "./containers/Helpers/Page500/";
+const store = configureStore();
 const history = createBrowserHistory();
-ReactDOM.render(
+/* console.log(window.location.pathname);
+ *//* var Path = getPath(window.location.pathname);
+ */ReactDOM.render(
   <Provider store={store} key="provider">
     <ErrorBoundary>
       <HashRouter history={history}>
         <Switch>
-          {/* <Route exact path="/500" name="Page 500" component={Page500}/> */}
-          {/* <Route path="/404" name="Page404" component={Page404} />
-          <Route exact path="" name="Home2" component={Approuter} />  */}
+          <Route exact path="/500" name="Page 500" component={Page500}/>
+          <Route path="/404" name="Page404" component={Page404} />
           <Route path="/" name="Home" component={Approuter} />
-          </Switch>
+         </Switch>
       </HashRouter>
     </ErrorBoundary>
   </Provider>,
